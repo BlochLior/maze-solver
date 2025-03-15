46b0032ec4fc849fe1ec8119611bd5b1c12f7fa4
BlochLior <blochlior@gmail.com>
2025-03-15 10:40:39 +0200

    added point class, added line class

--- graphics.py
+++ graphics.py
@@ -9,6 +9,9 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
+    def draw_line(self, line, fill_color=str):
+        line.draw(self.__canvas, fill_color)
+
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -21,3 +24,22 @@ class Window:
     
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_collor="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y,
+            self.p2.x, self.p2.y, fill=fill_collor,
+            width=2
+        )
+
+
