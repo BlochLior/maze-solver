e5f1f716d9c2d445e3623ec2d9ba4f47ced3014e
BlochLior <blochlior@gmail.com>
2025-03-16 09:48:46 +0200

    added wallbreaks, added reset visited property for cells after initial wallbreak

--- tests.py
+++ tests.py
@@ -38,5 +38,15 @@ class Tests(unittest.TestCase):
         self.assertFalse(
             m1._cells[m1._num_cols - 1][m1._num_rows - 1].has_bottom_wall
         )
+    
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertFalse(
+                    cell.visited
+                )
 if __name__ == "__main__":
     unittest.main()
